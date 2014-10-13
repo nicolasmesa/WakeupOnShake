28742f1c2700fcaccedf86e0613a4737859a84e2
Di Ruan <dr2763@columbia.edu>
2014-10-13 10:46:22 -0400

    checkpathch for test1.c

--- test/test1.c
+++ test/test1.c
@@ -13,31 +13,31 @@
 
 int set_acceleration(struct dev_acceleration  *acceleration)
 {
-        return syscall(__NR_set_acceleration, acceleration);
+	return syscall(__NR_set_acceleration, acceleration);
 }
 
 int accevt_create(struct acc_motion *acceleration)
 {
-        return syscall(__NR_accevt_create, acceleration);
+	return syscall(__NR_accevt_create, acceleration);
 }
 
 int accevt_wait(int event_id)
 {
-        return syscall(__NR_accevt_wait, event_id);
+	return syscall(__NR_accevt_wait, event_id);
 }
 
-int accevt_signal(struct dev_acceleration * acceleration)
+int accevt_signal(struct dev_acceleration *acceleration)
 {
-        return syscall(__NR_accevt_signal, acceleration);
+	return syscall(__NR_accevt_signal, acceleration);
 }
 
 int accevt_destroy(int event_id)
 {
-        return syscall(__NR_accevt_destroy, event_id);
+	return syscall(__NR_accevt_destroy, event_id);
 }
 
 
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
 	struct acc_motion myAcceleration;
 	struct dev_acceleration devAcc;
@@ -46,6 +46,5 @@ int main(int argc, char ** argv)
 	accevt_wait(10);
 	accevt_signal(&devAcc);
 	accevt_destroy(10);
-
-	return 0;			
+	return 0;
 }
